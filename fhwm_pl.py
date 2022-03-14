30aea16f8bf49a939b5a8711e142fa1645c32699
EndlessAssignment <83948372+EndlessAssignment@users.noreply.github.com>
2022-03-14 18:40:50 +0900

    온도에 따라 오름차순으로 정렬 스펙트럼이 있는 폴더만 작업 수행 공백 추가

--- fhwm_pl.py
+++ fhwm_pl.py
@@ -56,7 +56,7 @@ class Calc:
         return self.x_values[self.y_values.index(self.peak_height)]
 
 
-# 경로에서 데이터를 추출하는 함수
+# 경로에서 데이터를 추출하여 계산하는 함수
 def data_extract(folder, temperature, peak, laser):
     data = []
     file = glob(folder + '/' + temperature + '/spec/*mw.xlsx', recursive=True)
@@ -88,15 +88,16 @@ def data_extract(folder, temperature, peak, laser):
 
         except:
             # 오류 발생시 경로와 파워 출력
-            print(path, i)
+            print(temperature, i)
 
     return data
 
 
 # 계산된 데이터를 어레이로 변환하는 함수
-def data_array(data, folder,temperature, df_initial):
+def data_array(data, folder,temperature, df_integrated):
     dt_array = np.array(data)
-    names = ['Excitation Power (mW) - ' + temperature, 'light Output Power (a.u.)', 'FWHM (nm)',
+    df_empty = pd.DataFrame(index=range(0,1), columns=[''])
+    names = ['Excitation Power (mW) - ' + temperature, 'Light Output Power (a.u.)', 'FWHM (nm)',
              'Peak Wavelength (nm)', 'Photon Energy (eV)', 'EQE (a.u.)']
     df_data = pd.DataFrame(dt_array, columns=names)
 
@@ -107,20 +108,32 @@ def data_array(data, folder,temperature, df_initial):
     df_to_save.to_csv(folder + '/' + temperature + '/' + temperature + '_PL.csv', index=False)
 
     # 통합 데이터는 합쳐서 할당 해준다
-    df_initial = pd.concat([df_initial, df_to_save], axis=1)
+    df_integrated = pd.concat([df_integrated, df_to_save, df_empty], axis=1)
 
-    return df_initial
+    return df_integrated
 
 
-# 불러온 데이터를 저장하는 함수
+# 어레이를 csv 파일로 저장하는 함수
 def data_save(folder, peak, laser):
-    temp_list = os.listdir(folder)
-    df_total = pd.DataFrame()
-    for k in temp_list:
-        if k != "data.csv": # 한번 더 돌렸을 때 오류 방지용
-            data = data_extract(folder, k, peak, laser)
-            # 데이터 프레임 화
-            df_total = data_array(data, folder, k, df_total)
+    file_list = os.listdir(folder)  # 해당 경로 내에 있는 파일들
+    temp_list = []  # 온도 폴더들을 담을 리스트
+    df_total = pd.DataFrame()  # 전체 데이터를 담을 데이터 프레임
+
+    # 스펙트럼 데이터가 있는 폴더만 가져옴
+    for k in file_list:
+        spec = glob(folder + '/' + k + '/**/spec/*mw.xlsx', recursive=True)
+        if spec:
+            temp_list.append(int(k[:-1]))
+
+    # 온도에 따라 오름차순으로 정렬
+    temp_list.sort()
+
+    # 각 온도별 데이터 추출 및 저장 반복 작업
+    for l in temp_list:
+        temperature = str(l) + "K"
+        data = data_extract(folder, temperature, peak, laser)
+        # 데이터 프레임 화
+        df_total = data_array(data, folder, temperature, df_total)
 
     df_total.to_csv(folder + '/data.csv', index=False)
 
